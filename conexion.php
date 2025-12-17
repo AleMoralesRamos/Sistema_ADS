df01c840c2c457f6364e1a57fb14ea4c3a3f9a44
Alejandro Morales <alejandris2004@gmail.com>
2025-12-16 18:18:44 -0600

    act

--- conexion.php
+++ conexion.php
@@ -1,17 +1,17 @@
 <?php
 $servidor = "localhost";
 $usuario = "root";
-$password = "";  // Vacío en Linux si no configuraste contraseña
+$password = "";  
 $base_datos = "sistema_escolar";
 
-// Conectar a MySQL (sin especificar base de datos)
+// Conectar a MySQL 
 $conn = new mysqli($servidor, $usuario, $password);
 
 if ($conn->connect_error) {
     die("❌ Error de conexión MySQL: " . $conn->connect_error);
 }
 
-// Seleccionar la base de datos (crearla si no existe)
+// Selecciona la base de datos 
 $conn->query("CREATE DATABASE IF NOT EXISTS $base_datos");
 $conn->select_db($base_datos);
 
