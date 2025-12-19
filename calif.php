4be46dbc0a6f5b2de6a4a8b3d7626765d6428d39
Flaxcaster73 <tadeo.ipn8@gmail.com>
2025-12-18 23:06:28 -0600

    Create calif.php

---
+++ calif.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calificaciones</title>
+    <link rel="stylesheet" href="./css/estilo3.css">
+</head>
+<body>
+    <header>
+        <h1>Calificaciones Alumno</h1>
+    </header>
+    <main>
+        <section>
+            <article>
+                <?php
+                    require('./php/tabla.php');
+                ?>
+            </article>
+        </section>
+    </main>
+    <footer>
+        <p><strong></strong></p>
+    </footer>
+</body>
+</html>
